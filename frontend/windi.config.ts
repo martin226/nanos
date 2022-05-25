65b6f93eb0942d467cff51b40e9cff093f484ecf
Martin Sit <46298951+martin226@users.noreply.github.com>
2022-05-25 13:06:46 -0400

    feat: project setup

---
+++ frontend/windi.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from '@windicss/plugin-utils';
+
+export default defineConfig({
+  /**
+   * Write windi classes in html attributes.
+   * @see https://windicss.org/features/attributify.html
+   */
+});
