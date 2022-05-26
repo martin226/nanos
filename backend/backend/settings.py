e468d7bb10e8d88c61ef15a9ef7ab7403fc1b99f
Martin Sit <46298951+martin226@users.noreply.github.com>
2022-05-25 22:01:59 -0400

    fix: cors

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -45,9 +45,11 @@ INSTALLED_APPS = [
     "api",
     "rest_framework",
     "django_user_agents",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -133,3 +135,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CORS_ALLOW_ALL_ORIGINS = True
