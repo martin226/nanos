17b4a41bac47f518427f8cdc61fd4e96507eb04f
Martin Sit <46298951+martin226@users.noreply.github.com>
2022-05-29 23:01:20 -0400

    feat: link to home

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -3,7 +3,9 @@
     <div
       class="min-h-screen flex flex-col justify-center items-center space-y-6"
     >
-      <h1 class="text-7xl font-extralight">nanos</h1>
+      <NuxtLink to="/"
+        ><h1 class="text-7xl font-extralight">nanos</h1></NuxtLink
+      >
       <div class="px-2 w-full sm:w-4/5 md:w-3/4 lg:w-1/2 space-y-6">
         <FormCreate />
         <Nuxt />
