949b6cb620ca8322d944bfca19d1b8daaefbe9b9
Martin Sit <46298951+martin226@users.noreply.github.com>
2022-06-04 13:52:26 -0400

    fix: styling

--- frontend/components/Form/Create.vue
+++ frontend/components/Form/Create.vue
@@ -1,13 +1,19 @@
 <template>
-  <form class="flex divide-x-2 divide-zinc-700" @submit.prevent="createLink">
+  <form
+    class="flex divide-x-0 divide-y-2 md:(divide-y-0 divide-x-2) divide-zinc-700 flex-wrap"
+    @submit.prevent="createLink"
+  >
     <input
       ref="url"
       v-model="url"
       type="text"
       placeholder="https://example.com"
-      class="rounded-l-xl bg-zinc-800 text-zinc-300 focus:ring-gray-500 outline-none border-0 p-4 flex-grow"
+      class="rounded-t-xl md:(rounded-tr-none rounded-l-xl) bg-zinc-800 text-zinc-300 focus:ring-gray-500 outline-none border-0 p-4 flex-grow min-w-0"
     />
-    <button class="rounded-r-xl px-8 py-4 bg-zinc-800 text-zinc-400">
+    <button
+      type="submit"
+      class="w-full rounded-b-xl md:(w-min rounded-bl-none rounded-r-xl) px-8 py-4 bg-zinc-800 text-zinc-400"
+    >
       nano it!
     </button>
   </form>
