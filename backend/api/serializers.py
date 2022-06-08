13dd5a42433dfa5c968d858561ba61f63feaad87
Martin Sit <46298951+martin226@users.noreply.github.com>
2022-06-08 13:26:32 -0400

    refactor: remove redundant serializer

--- backend/api/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.serializers import ModelSerializer
-from .models import URLShorten
-
-
-class URLShortenSerializer(ModelSerializer):
-    class Meta:
-        model = URLShorten
-        fields = "__all__"
