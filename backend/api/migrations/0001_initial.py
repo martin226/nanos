6398d0d4043b0191972228924b0e39ed7da36864
Martin Sit <46298951+martin226@users.noreply.github.com>
2022-06-08 23:17:46 -0400

    fix: lint

--- backend/api/migrations/0001_initial.py
+++ backend/api/migrations/0001_initial.py
@@ -8,31 +8,53 @@ class Migration(migrations.Migration):
 
     initial = True
 
-    dependencies = [
-    ]
+    dependencies = []
 
     operations = [
         migrations.CreateModel(
-            name='URLShorten',
+            name="URLShorten",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('url', models.URLField(max_length=255)),
-                ('short_url', models.CharField(max_length=8, unique=True)),
-                ('created_at', models.DateTimeField(auto_now_add=True)),
-                ('updated_at', models.DateTimeField(auto_now=True)),
-                ('is_active', models.BooleanField(default=True)),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("url", models.URLField(max_length=255)),
+                ("short_url", models.CharField(max_length=8, unique=True)),
+                ("created_at", models.DateTimeField(auto_now_add=True)),
+                ("updated_at", models.DateTimeField(auto_now=True)),
+                ("is_active", models.BooleanField(default=True)),
             ],
         ),
         migrations.CreateModel(
-            name='View',
+            name="View",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('viewed_at', models.DateTimeField(auto_now_add=True)),
-                ('country', models.CharField(max_length=255, null=True)),
-                ('ip', models.GenericIPAddressField(null=True)),
-                ('referrer', models.CharField(max_length=255, null=True)),
-                ('device', models.CharField(max_length=255, null=True)),
-                ('short_url', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='views', to='api.urlshorten')),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("viewed_at", models.DateTimeField(auto_now_add=True)),
+                ("country", models.CharField(max_length=255, null=True)),
+                ("ip", models.GenericIPAddressField(null=True)),
+                ("referrer", models.CharField(max_length=255, null=True)),
+                ("device", models.CharField(max_length=255, null=True)),
+                (
+                    "short_url",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        related_name="views",
+                        to="api.urlshorten",
+                    ),
+                ),
             ],
         ),
     ]
